e0321a27be78b69991ae64b663f83a7237d7de92
Himanshuma38966 <74598469+Himanshuma38966@users.noreply.github.com>
2020-12-30 18:45:22 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,42 @@
+class Food {
+    constructor(){
+        this.foodStock=0;
+        this.lastFed;
+        this.image=loadImage("Milk.png");
+    }
+   getFoodStock(){
+       return this.foodStock;
+   } 
+   updateFoodStock(foodStock){
+       this.foodStock = foodStock;
+   } 
+   deductFood(){
+       if(this.foodStock>0){
+           this.foodStock = 60;
+       }
+   }
+   
+   
+
+   display(){
+        var x = 80,
+            y = 50;
+        imageMode(CENTER);
+        image(this.image,870,250,100,100);
+
+        
+       
+
+
+            for(var i=0; i < this.foodStock; i++){
+               if(i % 10 == 0){
+                   x = 80;
+                   y = y + 90;
+               }
+              image(this.image,x,y,100,100);
+              x = x + 50; 
+            }
+        
+   }
+   
+}
